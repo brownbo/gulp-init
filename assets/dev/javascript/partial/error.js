c20fe3a37a7898f96c4a93f81cd3f617cc13abe3
mrliang <619727242@qq.com>
2018-10-12 14:58:37 +0800

    2

--- assets/dev/javascript/partial/error.js
+++
@@ -1,6 +0,0 @@
-export default () => {
-  const pathName = location.pathname.split('/')
-  if(Number(pathName[pathName.length-1]) > 200) {
-    $('.x_'+Number(pathName[pathName.length-1])).show()
-  }
-}
